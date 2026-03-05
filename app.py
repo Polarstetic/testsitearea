29e98300e1e268d56ca293c3715b369709a3f187
Reeely <75444575+Polarstetic@users.noreply.github.com>
2026-03-05 04:20:44 -0500

    backend update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from google.oauth2 import service_account
 app = Flask(__name__)
 CORS(app)
 
-# load google credentials
+# load google credentials to be able to use the api
 google_creds_string = os.environ.get("GOOGLE_CREDENTIALS_JSON")
 if google_creds_string:
     creds_dict = json.loads(google_creds_string)
@@ -20,10 +20,10 @@ else:
 # website
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') # starts up the html
 
 # audio transcription
-@app.route('/api/transcribe', methods=['POST'])
+@app.route('/api/transcribe', methods=['POST']) # post method means we are sending the file to the google api
 def transcribe_audio():
     try:
         # get the audio file from the frontend request
@@ -33,27 +33,27 @@ def transcribe_audio():
         # package the audio and instructions for google
         audio = speech.RecognitionAudio(content=audio_content)
         config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.WEBM_OPUS,
-            sample_rate_hertz=48000,
-            language_code="en-US",
-            enable_automatic_punctuation=True, # allows for grammar to exist
+            encoding = speech.RecognitionConfig.AudioEncoding.WEBM_OPUS,
+            sample_rate_hertz = 48000,
+            language_code= "en-US",
+            enable_automatic_punctuation = True, # allows for grammar to exist
         )
 
-        # send to google
-        response = client.recognize(config=config, audio=audio)
+        # actually sending it to google
+        response = client.recognize(config = config, audio = audio)
 
-        # extract google response
+        # extract google response because it's messy
         transcript = ""
         for result in response.results:
             transcript += result.alternatives[0].transcript + "\n"
 
-        # wend the text back to the frontend
+        # send the text back to the frontend
         return jsonify({'text': transcript.strip()})
 
-    except Exception as e:
+    except Exception as e: # check
         print(f"Error: {e}")
         return jsonify({'error': 'Failed to transcribe'}), 500
 
-# start the server on Port 3000
+# start the server
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
